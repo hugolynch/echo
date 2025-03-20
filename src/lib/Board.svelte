984250b4ea563f78f204852955406ba61884ec05
Hugo Lynch <hugo@hopscotch.ca>
2025-03-20 18:38:41 -0400

    Initiate svelte

---
+++ src/lib/Board.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<div id="puzzle-display">
+    <!-- Puzzle clues will be displayed here -->
+  </div>
+  <div>
+    <input type="text" id="answer-input">
+    <button id="submit-button">Check Answer</button>
+  </div>
+  <p id="feedback"></p>
+
+<style>
+</style>
