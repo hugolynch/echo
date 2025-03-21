f12c94805d7fad6e6706252bac0db24eebc77d27
Hugo Lynch <hugo@hopscotch.ca>
2025-03-20 22:51:59 -0400

    Create svelte board and clue components, plus additional minor changes

--- src/lib/Board.svelte
+++ src/lib/Board.svelte
@@ -1,14 +1,106 @@
 <script>
+    import Clue from "./Clue.svelte";
+
+    const puzzle1 = {
+        name: "March 20, 2025 (Hugo)",
+        clues: [
+            {solution: "May", clues: [
+                {clues: [
+                    "one of the ",
+                    {clues: ["breathing"], solution: "living"},
+                    " ",
+                    {clues: [ "not breathing"], solution: "dead"},
+                ], solution: "zombie"},
+                " ",
+                {clues: [
+                    "in the ",
+                    {clues: ["roller coaster feature"], solution: "loop"},
+                ], solution: "aware"},
+                {clues: ["Loch ___ Monster"], solution: "ness"},
+                " month",
+            ]},
+            " the ",
+            {clues: [
+                {clues: [
+                    {clues: ["half a weekend"], solution: "Sunday"},
+                    " service",
+                ], solution: "mass"},
+                " ",
+                {clues: ["X, mathwise"], solution: "times"},
+                " accelerat",
+                {clues: ["charged particle"], solution: "ion"},
+            ], solution: "Force"},
+            " be with ",
+            {clues: [
+                "person ",
+                {clues: ["using Braille, say"], solution: "reading"},
+                " this ",
+                {clues: ["this is one"], solution: "clue"},
+            ], solution: "you"},
+        ],
+        solution: "May the Force be with you\n- a bunch of Jedi",
+    };
+
+    const puzzle2 = {
+        name: "March 20, 2025 (jean)",
+        clues: [
+            {clues: [
+                {clues: ["blessing"], solution: "benediction"},
+                " - ",
+                {clues: ["charged particle"], solution: "ion"},
+            ], solution: "Benedict"},
+            " ",
+            {clues: [
+                {clues: ["unlucky number"], solution: "thirteen"},
+                " in ",
+                {clues: ["city near Mount Vesuvis"], solution: "Pompeii"},
+            ], solution: "XIII"},
+            " is ",
+            {clues: ["choose"], solution: "elect"},
+            {clues: [
+                {clues: ["person fixing your typos, perhaps"], solution: "editor"},
+                ", for short",
+            ], solution: "ed"},
+            " ",
+            {clues: ["soda"], solution: "Pop"},
+            {clues: ["___=mc²"], solution: "e"},
+            " at the ",
+            {clues: [
+                {clues: ["Edge of ___"], solution: "seventeen"},
+                " in ",
+                {clues: ["you probably have twenty"], solution: "digits"}
+            ], solution: "17"},
+            {clues: ["film studio A___"], solution: "24"},
+            " ",
+            {clues: ["daddy"], solution: "papa"},
+            {clues: ["opposite of a W"], solution: "l"},
+            " ",
+            {clues: ["best adapted screenplay at the 9th Academy Awards"], solution: "conclave"},
+            ".",
+        ],
+        solution: "(on this day) Benedict XIII is elected Pope at the 1724 papal conclave.",
+    };
 </script>
 
-<div id="puzzle-display">
-    <!-- Puzzle clues will be displayed here -->
-  </div>
-  <div>
-    <input type="text" id="answer-input">
+<h1>{puzzle1.name}</h1>
+<div class="puzzle">
+    <Clue {...puzzle1} />
+</div>
+
+
+<h1>{puzzle2.name}</h1>
+<div class="puzzle">
+    <Clue {...puzzle2} />
+</div>
+
+<div>
+    <input type="text" id="answer-input" />
     <button id="submit-button">Check Answer</button>
-  </div>
-  <p id="feedback"></p>
+</div>
+<p id="feedback"></p>
 
 <style>
+    .puzzle {
+        margin: 1rem;
+    }
 </style>
---
+++ src/lib/Clue.svelte
@@ -0,0 +1,38 @@
+<script>
+    import Clue from './Clue.svelte';
+
+    let { solution, clues } = $props();
+    let solved = $state(false);
+</script>
+
+<span class="clue">
+    {#if solved}
+        <span class="solution">{solution}</span>
+    {:else}
+        {#each clues as clue}
+            {#if typeof clue === "string"}
+                {clue}
+            {:else}
+                <Clue {...clue} />
+            {/if}
+        {/each}
+    {/if}
+    <input type="checkbox" bind:checked={solved}>
+</span>
+
+<style>
+.clue {
+    display: inline-block;
+    border: 1px solid #58baf9;
+    border-radius: 5px;
+    background-color: oklch(from #58baf9 l c h / 0.2);
+    padding: 3px;
+    margin: 1px;
+}
+.solution {
+    white-space: pre;
+}
+input[type="checkbox"] {
+    min-width: inherit;
+}
+</style>
