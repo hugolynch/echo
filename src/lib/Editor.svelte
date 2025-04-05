aeec1770eec80788a08e8b03a94d558deb9dbba3
Hugo Lynch <hugo@hopscotch.ca>
2025-04-05 15:14:04 -0400

    Make editor copy to clipboard

--- src/lib/Editor.svelte
+++ src/lib/Editor.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
     import EditorClue from "./EditorClue.svelte"
-    let puzzle: {name: string, solution: string, clues: object[]} = $state({
-        name: "name", solution: "", clues: []
+
+    let copied = $state(false)
+    let puzzle: {name: string, date: string, solution: string, clues: object[]} = $state({
+        name: "name", date: "date", solution: "", clues: []
     })
-    // $inspect(puzzle)
 
     function exportPuzzle() {
-        console.log($state.snapshot({...puzzle, date: (new Date).toString()}))
+        console.debug($state.snapshot({...puzzle, date: (new Date).toString()}))
+        let promise = navigator.clipboard.writeText(JSON.stringify(puzzle))
+        promise.then(() => {
+            copied = true
+            setTimeout(() => {
+                copied = false
+          }, 2000);
+        })
     }
 </script>
 
 <h1>Editor <span>BETA</span></h1>
 <EditorClue bind:solution={puzzle.solution} bind:clues={puzzle.clues} />
-<button onclick={exportPuzzle}>Export (to console)</button>
+<button onclick={exportPuzzle}>
+    {#if !copied}
+        Copy to Clipboard
+    {:else}
+        Copied to Clipboard ✅
+    {/if}
+</button>
 
 <style>
     h1 {
