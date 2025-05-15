716692ae2e13b13264bd18220ef0f277bddaa9c4
Jean Lynch <thomas.jean.lynch@gmail.com>
2025-05-15 19:32:40 -0400

    Delete editor
    Will create a new standalone project later with the editor code.

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,8 @@
 <script lang="ts">
   import Board from "./components/Board.svelte";
-  import Editor from "./components/Editor.svelte";
-
-  let editing = JSON.parse(localStorage.getItem('editing') ?? "false")
-
-  function toggle() {
-    editing = !editing
-    localStorage.setItem('editing', JSON.stringify(editing))
-  }
 </script>
 
-{#if editing}
-<Editor />
-{:else}
 <Board />
-{/if}
-
-<button onclick={toggle}>
-  {#if editing}
-  Puzzles →
-  {:else}
-  Editor →
-  {/if}
-</button>
 
 <style>
-  button {
-    margin: 0 auto;
-    padding: 8px;
-  }
 </style>
